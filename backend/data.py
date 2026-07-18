967bc9540c550172f208bdf8098b0a2bdb7b4720
Krunner1015 <kaidenbartrum15@gmail.com>
2026-07-17 22:29:44 -0400

    Initial commit

---
+++ backend/data.py
@@ -0,0 +1,6 @@
+DISNEY_PARKS = {
+    6: "Magic Kingdom",
+    5: "EPCOT",
+    7: "Hollywood Studios",
+    8: "Animal Kingdom"
+}
---
+++ backend/main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+DISNEY_PARKS = {
+    6: "Magic Kingdom",
+    5: "EPCOT",
+    7: "Hollywood Studios",
+    8: "Animal Kingdom"
+}
+
+@app.get("/parks")
+def get_parks():
+    return DISNEY_PARKS
+
+@app.get("waittimes/{park_id}")
+def get_wait_times(park_id: int):
+    url = f"https://queue-times.com/parks/{park_id}/queue_times.json"
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    result = []
+
+    for land in data["lands"]:
+        rides = []
+
+        for ride in land["rides"]:
+            rides.append({
+                "name": ride["name"], 
+                "wait_time": ride["wait_time"], 
+                "open": ride["is_open"]
+            })
+        
+        result.append({
+            "land": land["name"], 
+            "rides": rides
+        })
+    
+    return {
+        "park": DISNEY_PARKS[park_id], 
+        "lands": result
+    }
